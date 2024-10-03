a425472041ea6ebeb5dd315cbca6271ed69878b4
reuzed <49994279+reuzed@users.noreply.github.com>
2024-10-03 15:20:55 +0100

    Addition api

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!, Here is another hello'
 
-@app.route('/hi')
-def hi():
-    return '<h1>Here\'s a different message!<h1>'
+@app.route('/add/<x>/<y>')
+def hi(x,y):
+    added = x + y
+    return '<h1>Here\'s a different message!<h1> <br/> The result of the addition is ' + str(added)
