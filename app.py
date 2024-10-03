61cea8f77d9420aa7ad5681f650ef7a78eeaafe7
reuzed <49994279+reuzed@users.noreply.github.com>
2024-10-03 15:09:38 +0100

    Another hi

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!, Here is another hello'
+
+@app.route('/hi')
+def hello_world():
+    return 'Here\'s a different message!'
